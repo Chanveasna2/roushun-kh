d6e2b8f294c13674c1e05f3a2528bc79d1a3ec1a
Chanveasna2 <hchanveasna@gmail.com>
2018-09-25 13:07:05 +0700

    Security Login

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,9 @@
 <?php
-
+Route::get('/',function (){
+   return view('welcome');
+});
 Route::get('/admin','DashboardController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
