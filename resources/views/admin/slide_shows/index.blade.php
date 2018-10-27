e4d1fe4a8ed2d2eb0e876916f2ea3d0f91d66c2b
soklim <soklim.kheng@gmail.com>
2018-10-27 17:07:35 +0700

    datatable

--- resources/views/admin/slide_shows/index.blade.php
+++ resources/views/admin/slide_shows/index.blade.php
@@ -16,49 +16,54 @@
     @endif
     <!-- Page Content -->
         <a href="{{route('slide_shows.create')}}" class="color:white;"><button class="btn btn-primary">Create</button></a>
-        <table class="table">
-            <thead class="thead-dark">
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Slide Name</th>
-                <th scope="col">Order</th>
-                <th scope="col">Image</th>
-                <th scope="col">Created</th>
-                <th scope="col">Updated</th>
-                <th scope="col">Action</th>
-                <th scope="col"></th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($slide_shows as $slide_show)
-                <tr>
-                    <th scope="row">{{$slide_show->id}}</th>
-                    <td>{{$slide_show->slide_name}}</td>
-                    <td>{{$slide_show->order}}</td>
-                    <td><img height="50px;" src="{{$slide_show->photo?$slide_show->photo->file:'https://via.placeholder.com/400x400'}}" alt=""></td>
-                    <td>{{$slide_show->created_at->diffForHumans()}}</td>
-                    <td>{{$slide_show->updated_at}}</td>
-                    <td>
-                        <a href="{{route('slide_shows.edit',$slide_show->id)}}"><i class="btn btn-primary fas fa-edit"></i></a>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Slide Name</th>
+                        <th scope="col">Order</th>
+                        <th scope="col">Image</th>
+                        <th scope="col">Created</th>
+                        <th scope="col">Updated</th>
+                        <th scope="col">Action</th>
+                        <th scope="col"></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($slide_shows as $slide_show)
+                        <tr>
+                            <th scope="row">{{$slide_show->id}}</th>
+                            <td>{{$slide_show->slide_name}}</td>
+                            <td>{{$slide_show->order}}</td>
+                            <td><img height="50px;" src="{{$slide_show->photo?$slide_show->photo->file:'https://via.placeholder.com/400x400'}}" alt=""></td>
+                            <td>{{$slide_show->created_at->diffForHumans()}}</td>
+                            <td>{{$slide_show->updated_at}}</td>
+                            <td>
+                                <a href="{{route('slide_shows.edit',$slide_show->id)}}"><i class="btn btn-primary fas fa-edit"></i></a>
 
-                    </td>
-                    <td>
-                        {!! Form::open(['method'=>'DELETE','action'=>['SlideShowController@destroy',$slide_show->id]]) !!}
-                        <div class="form-group ">
-                            {{--{!! Form::submit('',['class'=>'btn btn-danger fas fa-edit']) !!}--}}
-                            <button class="btn btn-danger fas fa-trash-alt" type="submit" value=""></button>
-                        </div>
-                        {!! Form::close() !!}
-                    </td>
-                </tr>
-            @endforeach
+                            </td>
+                            <td>
+                                {!! Form::open(['method'=>'DELETE','action'=>['SlideShowController@destroy',$slide_show->id]]) !!}
+                                <div class="form-group ">
+                                    {{--{!! Form::submit('',['class'=>'btn btn-danger fas fa-edit']) !!}--}}
+                                    <button class="btn btn-danger fas fa-trash-alt" type="submit" value=""></button>
+                                </div>
+                                {!! Form::close() !!}
+                            </td>
+                        </tr>
+                    @endforeach
 
-            </tbody>
-        </table>
-
+                    </tbody>
+                </table>
+            </div>
+        </div>
 
     </div>
     <!-- /.container-fluid -->
     </div>
     </div>
+
+
 @stop
